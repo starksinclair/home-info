8b967723fe5638be77f06135a5c7ebbd097e71f8
Sinclair Nzenwata <83012040+starksinclair@users.noreply.github.com>
2024-12-22 22:41:20 -0500

    looking for bug

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server"
 
 const API_KEY = process.env.API_KEY
+if (!API_KEY) {
+    throw new Error("API_KEY is not defined in environment variables");
+  }
 export async function GET() {
    const weather = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=Muncie&days=1&aqi=no&alerts=no`) 
     const quote = await fetch(`https://labs.bible.org/api/?formatting=plain&passage=votd`)
@@ -9,5 +12,6 @@ export async function GET() {
     const weatherData = await weather.json()
     const quoteData = await quote.text()
     const data = {weather: weatherData, quote: quoteData}
+    console.log(data)
     return NextResponse.json(data);
 }
